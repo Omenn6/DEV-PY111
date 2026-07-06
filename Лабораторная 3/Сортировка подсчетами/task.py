4776e753f0715f86b93100d21a7fbc4ae8d14734
Pavel <bruchkovskiiy@mail.ru>
2026-07-06 13:19:14 +0300

    lab_3

--- Лабораторная 3/Сортировка подсчетами/task.py
+++ Лабораторная 3/Сортировка подсчетами/task.py
@@ -12,4 +12,21 @@ def sort(container: Sequence[int]) -> Sequence[int]:
     :param container: Массив, который надо отсортировать
     :return: Отсортированный в порядке возрастания массив
     """
-    ...  # TODO реализовать алгоритм сортировки подсчетами
+    if not container:
+        return []
+
+    max_val = container[0]
+    for num in container:
+        if num > max_val:
+            max_val = num
+
+    count_arr = [0] * (max_val + 1)
+
+    for num in container:
+        count_arr[num] += 1
+
+    sorted_arr = []
+    for num, count in enumerate(count_arr):
+        sorted_arr.extend([num] * count)
+
+    return sorted_arr  # TODO реализовать алгоритм сортировки подсчетами
